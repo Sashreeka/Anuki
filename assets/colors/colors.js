bfb54a61bc265a39598c60d583d597474f55ec94
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-07-03 04:48:05 +0530

    color palet of project

---
+++ assets/colors/colors.js
@@ -0,0 +1,10 @@
+const colors={
+    background:"#FFFFFF",
+    backgroundAsh:"#F0F0F0",
+    primary:"#A5E175",
+    secondary:"#B7F785",
+    textlight:"#969696",
+    textDark:"#000000",
+};
+
+export default colors;
