b48bf910e797cc98e10a9bcf31a40d9d87a85274
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-07-03 05:24:34 +0530

    clear test content

--- App.js
+++ App.js
@@ -1,33 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import colors from "./assets/colors/colors"
+import colors from "./assets/colors/colors";
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default function App() {
   return (
-    <View style={styles.container}>
       <Text>Shashrika app eka</Text>
-      <Text>This is the first react APP</Text>
-        <View style={styles.container2}>
-        <Text>Shashrika app eka</Text>
-        <Text>Oya hariyata wada karanawada please</Text>
-      </View>
-    <StatusBar style="auto" />
-    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    
-  },
-  container2: {
-    backgroundColor: '#eee',
-    fontFamily:"Roboto-BoldItalic",
-    fontSize:40,
-  },
-});
