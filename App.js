7539050c1aa7ed9ac7db6812596bafd99a55f428
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-07-03 14:01:35 +0530

    home page , header dissapier

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import colors from "./assets/colors/colors";
-import Icon from 'react-native-vector-icons/Ionicons';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import {Text} from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from "./components/Home";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-      <Text>Shashrika app eka</Text>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} 
+        options={{headerShown: false,}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
