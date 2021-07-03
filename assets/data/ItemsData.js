a35d39ea0645a887484f4cf2bd7b093753e1ad8a
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-07-03 14:03:59 +0530

    bug fixed

--- assets/data/ItemsData.js
+++ assets/data/ItemsData.js
@@ -1,37 +1,40 @@
-const pulularData=[
+const itemsData=[
     {
         id:1,
-        image: require("../images/bio1"),
+        image: require("../images/bio1.png"),
         title: "Bio Vaccine",
         description: "Bio VaccineBio VaccineBio VaccineBio VaccineBio Vaccine",
         rating: 5.0,  
     },
     {
         id:2,
-        image: require("../images/bio2"),
+        image: require("../images/bio2.png"),
         title: "Bio Gold",
         description: "Bio GoldBio GoldBio GoldBio Gold",
         rating: 4.3,  
     },
     {
         id:3,
-        image: require("../images/bio3"),
+        image: require("../images/bio3.png"),
         title: "Bio Phos",
         description: "Bio PhosBio PhosBio Phos Bio Phos",
         rating: 4.3,  
     },
     {
         id:4,
-        image: require("../images/bio1"),
+        image: require("../images/bio1.png"),
         title: "Bio Vaccine",
         description: "Bio PhosBio PhosBio PhosBio Phos",
         rating: 4.3,  
     },
     {
         id:5,
-        image: require("../images/bio1"),
+        image: require("../images/bio1.png"),
         title: "Bio Vaccine",
         description: "Bio PhosBio PhosBio PhosBio PhosBio PhosBio Phos",
         rating: 4.3,  
     },
-]
+];
+
+
+export default itemsData;
