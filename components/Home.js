75fc1bc64f296fc77f1e5f263044380377397a34
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-07-03 16:10:59 +0530

    display categories function - step 1

--- components/Home.js
+++ components/Home.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {View, Text, StyleSheet,SafeAreaView,Image} from "react-native";
+import {FlatList,View, Text, StyleSheet,SafeAreaView,Image} from "react-native";
 import Feather from "react-native-vector-icons/Feather";
 import fontAwsome from "react-native-vector-icons/FontAwesome";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
@@ -8,8 +8,18 @@ import categoriesData from '../assets/data/categoriesData';
 import itemsData from '../assets/data/itemsData';
 import colors from '../assets/colors/colors';
 
+
 Feather.loadFont();
 export default Home = ()=>{
+
+    const renderCategoryItem=(item)=>{
+        return(
+            <View style={styles.categoriesListWrapper}>
+                <Text>hello</Text>
+            </View>
+        );
+    };
+
     return(
         <View style={styles.container}>
             <SafeAreaView>
@@ -20,7 +30,31 @@ export default Home = ()=>{
                 
             </SafeAreaView>
             {/* titles */}
-            
+            <View style={styles.titleView}>
+                <Text style={styles.titlesTitle}>Organic Fertilizer</Text>
+            </View>
+
+            {/* Search */}
+            <View style={styles.searchWrapper}>
+                <Feather name="search" size={16} color={colors.textDark}/>
+                <View style={styles.search}>
+                    <Text style={styles.searchText}>Search...</Text>
+                </View>
+            </View>
+
+            {/* Categories */}
+            <View style={styles.categoriesWrapper}>
+                <Text style={styles.categoriesTitle}>Categories</Text>
+                <View style={styles.categoriesListWrapper}>
+                <FlatList
+                  data={categoriesData}
+                  renderItem={renderCategoryItem}
+                  keyExtractor={item => item.id}
+                  horizontal={true}
+                  />
+                
+                </View> 
+            </View>
         </View>
         
     );
@@ -36,7 +70,6 @@ const styles=StyleSheet.create({
         justifyContent:"space-between",
         paddingHorizontal:20,
         paddingTop:35,
-        paddingBottom:10,
         alignItems:"center",
         backgroundColor:colors.primary,
     },
@@ -46,5 +79,52 @@ const styles=StyleSheet.create({
         borderRadius:40,
         
     },
+    profileImage:{
+        width:40,
+        height:40,
+        borderRadius:40,
+        
+    },
+    titlesTitle:{
+        // fontFamily:"Roboto-bold",
+        fontSize:32,
+        color:colors.textDark,
+        paddingLeft:10,
+    },
+    titleView:{
+        backgroundColor:colors.primary,
+        padding:10,
+        paddingBottom:10,
+        borderBottomLeftRadius:40,
+        borderBottomRightRadius:40,
+    },
+    searchWrapper:{
+        paddingTop:20,
+        paddingBottom:10,
+        flexDirection:"row",
+        alignItems:"center",  
+        borderBottomColor:colors.textlight,
+        borderBottomWidth:2,
+        marginHorizontal:20,
+    },
+    search:{
+        marginLeft:20,
+    },
+    searchText:{
+        fontSize:15,
+        color:colors.textlight,
+    },
+    categoriesWrapper:{
+       marginTop:30,
+
+    },
+    categoriesTitle:{
+        fontWeight:"bold",
+        fontSize:20,
+        paddingHorizontal:20,
+    },
+    // categoriesListWrapper:{
+
+    // },
 
 })
