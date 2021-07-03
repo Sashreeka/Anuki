48fbfa9ebcd243ef364f8bfe2307937b38124336
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-07-03 09:54:51 +0530

    item data - hardcoded

---
+++ assets/data/ItemsData.js
@@ -0,0 +1,37 @@
+const pulularData=[
+    {
+        id:1,
+        image: require("../images/bio1"),
+        title: "Bio Vaccine",
+        description: "Bio VaccineBio VaccineBio VaccineBio VaccineBio Vaccine",
+        rating: 5.0,  
+    },
+    {
+        id:2,
+        image: require("../images/bio2"),
+        title: "Bio Gold",
+        description: "Bio GoldBio GoldBio GoldBio Gold",
+        rating: 4.3,  
+    },
+    {
+        id:3,
+        image: require("../images/bio3"),
+        title: "Bio Phos",
+        description: "Bio PhosBio PhosBio Phos Bio Phos",
+        rating: 4.3,  
+    },
+    {
+        id:4,
+        image: require("../images/bio1"),
+        title: "Bio Vaccine",
+        description: "Bio PhosBio PhosBio PhosBio Phos",
+        rating: 4.3,  
+    },
+    {
+        id:5,
+        image: require("../images/bio1"),
+        title: "Bio Vaccine",
+        description: "Bio PhosBio PhosBio PhosBio PhosBio PhosBio Phos",
+        rating: 4.3,  
+    },
+]
